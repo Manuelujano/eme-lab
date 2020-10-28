737aa4347c0fbd4f535b106c532c82c23ddd3c87
Manuelujano <aketzali20@gmail.com>
2020-10-28 10:44:05 -0600

    docs add

---
+++ docs/precache-manifest.9ddbbf91183e455f3581eb07cfde3c80.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac1e0bb28e7f5a00f418afb3dfe961c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a604f0906135f686b836",
+    "url": "/static/css/main.befbcc0f.chunk.css"
+  },
+  {
+    "revision": "6c24ece7305b78c95324",
+    "url": "/static/js/2.032d5063.chunk.js"
+  },
+  {
+    "revision": "028d1b8f21ffff8caf0c799a051bd2b8",
+    "url": "/static/js/2.032d5063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a604f0906135f686b836",
+    "url": "/static/js/main.e351d41c.chunk.js"
+  },
+  {
+    "revision": "0fa0a075b4cd4e468928",
+    "url": "/static/js/runtime-main.7d4ec9a6.js"
+  },
+  {
+    "revision": "e95593101a7dc0eb0a91806b51b7cbf8",
+    "url": "/static/media/NeueMontreal-Regular.e9559310.otf"
+  },
+  {
+    "revision": "b5a18a611d84b6611535c4b1c4af2162",
+    "url": "/static/media/NeueMontreal-RegularItalic.b5a18a61.otf"
+  },
+  {
+    "revision": "e1ba326c3b167023ba3d82715c6d331f",
+    "url": "/static/media/logoblanco.e1ba326c.png"
+  },
+  {
+    "revision": "d6dd11833c0adfb473f3690fa84df6db",
+    "url": "/static/media/pegaso.d6dd1183.png"
+  }
+]);
